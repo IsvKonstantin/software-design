27d44ad73c97d47b4a0664e7261ac862978fbf91
Isaev Konstantin <isvkonstantin@gmail.com>
2021-11-23 16:52:14 +0300

    [HW5] updated build.gradle, added JavaAwtDrawingApi.kt and JavaFxDrawingApi.kt implementation

--- bridge/src/main/kotlin/drawing/JavaAwtDrawingApi.kt
+++ bridge/src/main/kotlin/drawing/JavaAwtDrawingApi.kt
@@ -1,4 +1,69 @@
 package drawing
 
-class JavaAwtDrawingApi {
+import graph.Circle
+import graph.Line
+import java.awt.Color
+import java.awt.Frame
+import java.awt.Graphics
+import java.awt.Graphics2D
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
+import java.awt.geom.Ellipse2D
+import java.awt.geom.Line2D
+import kotlin.system.exitProcess
+
+class JavaAwtDrawingApi : DrawingApi, Frame() {
+    private val circles = mutableListOf<Ellipse2D.Double>()
+    private val lines = mutableListOf<Line2D.Double>()
+
+    override val drawingAreaWidth = 1000
+
+    override val drawingAreaHeight = 1000
+
+    override fun drawCircle(circle: Circle) {
+        circles.add(
+            Ellipse2D.Double(
+                circle.center.x - circle.radius,
+                circle.center.y - circle.radius,
+                circle.radius * 2,
+                circle.radius * 2
+            )
+        )
+    }
+
+    override fun drawLine(line: Line) {
+        lines.add(
+            Line2D.Double(
+                line.from.x,
+                line.from.y,
+                line.to.x,
+                line.to.y
+            )
+        )
+    }
+
+    override fun show() {
+        addWindowListener(object : WindowAdapter() {
+            override fun windowClosing(windowEvent: WindowEvent) {
+                dispose()
+                exitProcess(0)
+            }
+        })
+
+        setSize(drawingAreaWidth, drawingAreaHeight)
+        isVisible = true
+    }
+
+    override fun paint(g: Graphics?) {
+        val graphics2D = g as Graphics2D
+        graphics2D.paint = Color.black
+
+        lines.forEach {
+            graphics2D.draw(it)
+        }
+
+        circles.forEach {
+            graphics2D.fill(it)
+        }
+    }
 }
--- bridge/src/main/kotlin/drawing/JavaFxDrawingApi.kt
+++ bridge/src/main/kotlin/drawing/JavaFxDrawingApi.kt
@@ -1,4 +1,39 @@
 package drawing
 
-class JavaFxDrawingApi {
+import javafx.application.Application
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.scene.shape.Circle
+import javafx.scene.shape.Line
+import javafx.scene.shape.Shape
+import javafx.stage.Stage
+import graph.Circle as MyCircle
+import graph.Line as MyLine
+
+class JavaFxDrawingApi : DrawingApi, Application() {
+    companion object {
+        private val shapes: MutableList<Shape> = ArrayList()
+    }
+
+    override val drawingAreaWidth = 800
+
+    override val drawingAreaHeight = 600
+
+    override fun drawCircle(circle: MyCircle) {
+        shapes.add(Circle(circle.center.x, circle.center.y, circle.radius))
+    }
+
+    override fun drawLine(line: MyLine) {
+        shapes.add(Line(line.from.x, line.from.y, line.to.x, line.to.y))
+    }
+
+    override fun show() = launch()
+
+    override fun start(stage: Stage?) {
+        stage?.run {
+            val root: Group = Group().apply { this.children.addAll(shapes) }
+            scene = Scene(root, drawingAreaWidth.toDouble(), drawingAreaHeight.toDouble())
+            show()
+        }
+    }
 }
