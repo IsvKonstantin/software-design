66a19a58e883867a4e227323c86ee9349d25e3cd
Isaev Konstantin <isvkonstantin@gmail.com>
2021-12-01 21:41:47 +0300

    [HW6] implemented CalcVisitorTest

--- calculator/src/main/kotlin/visitor/СalcVisitor.kt
+++ calculator/src/main/kotlin/visitor/СalcVisitor.kt
@@ -1,19 +1,37 @@
 package visitor
 
-import token.Brace
-import token.NumberToken
-import token.Operation
+import token.*
+import java.util.*
 
 class CalcVisitor : TokenVisitor {
+    private val stack: Deque<Double> = ArrayDeque()
+
     override fun visit(token: NumberToken) {
-        TODO("Not yet implemented")
+        stack.push(token.value.toDouble())
     }
 
     override fun visit(token: Brace) {
-        TODO("Not yet implemented")
+        throw RuntimeException("Found brace in RPN")
     }
 
     override fun visit(token: Operation) {
-        TODO("Not yet implemented")
+        if (stack.size < 2) {
+            throw RuntimeException("Invalid expression")
+        }
+
+        val y = stack.removeFirst()
+        val x = stack.removeFirst()
+
+        when(token.type) {
+            OperationType.PLUS -> stack.push(x + y)
+            OperationType.MINUS -> stack.push(x - y)
+            OperationType.MUL -> stack.push(x * y)
+            OperationType.DIV -> stack.push(x / y)
+        }
+    }
+
+    fun evaluate(tokens: List<Token>) : Double {
+        tokens.forEach { it.accept(this) }
+        return stack.poll() ?: throw RuntimeException("Invalid expression")
     }
 }
