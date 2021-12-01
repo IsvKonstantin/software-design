a7892356dbd87f0d829342b6620bed88ce58e9a2
Isaev Konstantin <isvkonstantin@gmail.com>
2021-12-01 21:41:59 +0300

    [HW6] added tests for CalcVisitor

---
+++ calculator/src/test/kotlin/visitor/CalcVisitorTest.kt
@@ -0,0 +1,68 @@
+package visitor
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import token.Brace
+import token.NumberToken
+import token.Operation
+
+internal class CalcVisitorTest {
+    @Test
+    fun `CalcVisitor simple test`() {
+        val calcVisitor = CalcVisitor()
+        val tokens = listOf(
+            NumberToken(10),
+            NumberToken(5),
+            Operation('+'),
+        )
+
+        assertThat(calcVisitor.evaluate(tokens)).isEqualTo(15.0)
+    }
+
+    @Test
+    fun `CalcVisitor complex test`() {
+        val calcVisitor = CalcVisitor()
+        val tokens = listOf(
+            NumberToken(10),
+            NumberToken(5),
+            NumberToken(6),
+            NumberToken(2),
+            Operation('/'),
+            NumberToken(3),
+            Operation('-'),
+            Operation('*'),
+            Operation('+'),
+        )
+
+        assertThat(calcVisitor.evaluate(tokens)).isEqualTo(10.0)
+    }
+
+    @Test
+    fun `CalcVisitor invalid expression`() {
+        val calcVisitor = CalcVisitor()
+        val tokens = listOf(
+            NumberToken(5),
+            Operation('+')
+        )
+
+        Assertions.assertThatThrownBy { calcVisitor.evaluate(tokens) }
+            .isInstanceOf(RuntimeException::class.java)
+            .hasMessage("Invalid expression")
+    }
+
+    @Test
+    fun `CalcVisitor has brace in input tokens`() {
+        val calcVisitor = CalcVisitor()
+        val tokens = listOf(
+            NumberToken(5),
+            NumberToken(5),
+            Operation('+'),
+            Brace(')')
+        )
+
+        Assertions.assertThatThrownBy { calcVisitor.evaluate(tokens) }
+            .isInstanceOf(RuntimeException::class.java)
+            .hasMessage("Found brace in RPN")
+    }
+}
